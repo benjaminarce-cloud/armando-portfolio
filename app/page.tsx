0c4892e3ec8e211af7bf4d5c9f907d14e055a6a7
benjaminarce-cloud <arceb3013@gmail.com>
2026-01-12 13:47:05 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import HomeHero from "@/components/home/HomeHero";
-import HomeMenu from "@/components/home/HomeMenu";
+import WorkMenu from "@/components/home/WorkMenu";
 import HomeEndCard from "@/components/home/HomeEndCard";
 
 export default function Page() {
   return (
     <main className="bg-[color:var(--page-bg)] text-[color:var(--page-fg)]">
       <HomeHero />
-      <HomeMenu />
+      <WorkMenu />
       <HomeEndCard />
     </main>
   );
