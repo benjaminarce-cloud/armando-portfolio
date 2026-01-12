577b379417974923dd63fa4ed7d4db066f532807
benjaminarce-cloud <arceb3013@gmail.com>
2026-01-12 11:30:40 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,9 @@ import HomeEndCard from "@/components/home/HomeEndCard";
 
 export default function Page() {
   return (
-    <main className="bg-[var(--page-bg)] text-[var(--page-fg)]">
+    <main className="bg-[color:var(--page-bg)] text-[color:var(--page-fg)]">
       <HomeHero />
-
-      {/* Netflix-style spotlight row (vertical reels + hover/center spotlight) */}
       <SpotlightRail />
-
-      {/* End card (white A24-ish footer) */}
       <HomeEndCard />
     </main>
   );
