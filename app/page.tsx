97dbf32a786b91959d229af84ce2a10e986b4a62
benjaminarce-cloud <arceb3013@gmail.com>
2026-01-12 13:08:42 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import HomeHero from "@/components/home/HomeHero";
-import SpotlightRail from "@/components/home/SpotlightRail";
+import HomeMenu from "@/components/home/HomeMenu";
 import HomeEndCard from "@/components/home/HomeEndCard";
 
 export default function Page() {
   return (
-<main className="bg-[color:var(--page-bg)] text-[color:var(--page-fg)]">
+    <main className="bg-[color:var(--page-bg)] text-[color:var(--page-fg)]">
       <HomeHero />
-      <SpotlightRail />
+      <HomeMenu />
       <HomeEndCard />
     </main>
   );
